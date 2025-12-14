71fa6f99705e915794057415f1966fb80d86c950
Ruixuan <ruixuanl@andrew.cmu.edu>
2025-12-13 23:45:27 -0500

    add env

--- src/py_scripts/Env.py
+++ src/py_scripts/Env.py
@@ -4,7 +4,14 @@ class Environment():
     def __init__(self, config_fname):
         # Load Config Settings
         config = load_json(config_fname)
-        
+        self.lego_lib = load_json(config["Lego_Library_fname"])
+        self.workspace_range = config["Workspace_Range"]
+        self.storage_on_plate = config["Storage_On_Plate"]
+        self.objects_T = {}
+        self.brick_tall = 0.0096
+        self.plate_tall = 0.0016
+        self.P_len = 0.008
+
         # Create the service client
         rospy.wait_for_service('/gazebo/set_model_state')
         self.set_state_client = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
@@ -14,18 +21,57 @@ class Environment():
     
     def init_env(self, fname):
         env_setup = load_json(fname)
+
         storage_setup = env_setup["storage_plate"]
-        r = R.from_euler('xyz', [storage_setup["roll"], storage_setup["pitch"], storage_setup["yaw"]]).as_quat()
-        pose = [storage_setup["x"], storage_setup["y"], storage_setup["z"], r[0], r[1], r[2], r[3]]
+        self.storage_plate_dim = [storage_setup["height"], storage_setup["width"]]
+        storage_plate_T = np.eye(4)
+        r = R.from_euler('xyz', [storage_setup["roll"], storage_setup["pitch"], storage_setup["yaw"]])
+        storage_plate_T[:3, :3] = r.as_matrix()
+        storage_plate_T[:3, 3] = np.array([storage_setup["x"], storage_setup["y"], storage_setup["z"]])
+        quat = r.as_quat()
+        pose = [storage_setup["x"], storage_setup["y"], storage_setup["z"], quat[0], quat[1], quat[2], quat[3]]
         self.set_state("storage_plate", pose)
+        self.objects_T["storage_plate"] = storage_plate_T
 
         assemble_setup = env_setup["assemble_plate"]
-        r = R.from_euler('xyz', [assemble_setup["roll"], assemble_setup["pitch"], assemble_setup["yaw"]]).as_quat()
-        pose = [assemble_setup["x"], assemble_setup["y"], assemble_setup["z"], r[0], r[1], r[2], r[3]]
+
+        assemble_plate_T = np.eye(4)
+        self.assemble_plate_dim = [assemble_setup["height"], assemble_setup["width"]]
+        r = R.from_euler('xyz', [assemble_setup["roll"], assemble_setup["pitch"], assemble_setup["yaw"]])
+        assemble_plate_T[:3, :3] = r.as_matrix()
+        assemble_plate_T[:3, 3] = np.array([assemble_setup["x"], assemble_setup["y"], assemble_setup["z"]])
+        quat = r.as_quat()
+        pose = [assemble_setup["x"], assemble_setup["y"], assemble_setup["z"], quat[0], quat[1], quat[2], quat[3]]
         self.set_state("assemble_plate", pose)
+        print(pose)
+        self.objects_T["assemble_plate"] = assemble_plate_T
 
-
-
+        for k in env_setup.keys():
+            if(k == "storage_plate" or k == "assemble_plate"):
+                continue
+            brick_setup = env_setup[k]
+            brick_T = np.eye(4)
+            if(self.storage_on_plate):
+                x_on_plate, y_on_plate, z_on_plate, ori = brick_setup["x"], brick_setup["y"], brick_setup["z"], brick_setup["ori"]
+                brick_T = calc_brick_T(k, x_on_plate, y_on_plate, z_on_plate, ori, storage_plate_T, self.storage_plate_dim, self.lego_lib, P_len=self.P_len, brick_tall=self.brick_tall)
+                r = R.from_matrix(brick_T[:3, :3])
+                quat = r.as_quat()
+                pose = [brick_T[0, 3], brick_T[1, 3], brick_T[2, 3], quat[0], quat[1], quat[2], quat[3]]
+                self.set_state(k, pose)
+            else:
+                x, y = np.random.uniform(self.workspace_range["x_min"], self.workspace_range["x_max"]), np.random.uniform(self.workspace_range["y_min"], self.workspace_range["y_max"])
+                z = self.workspace_range["z"] + self.brick_tall - self.plate_tall
+                roll, pitch, yaw = storage_setup["roll"], storage_setup["pitch"], np.random.uniform(-np.pi, np.pi)
+                r = R.from_euler('xyz', [roll, pitch, yaw])
+                quat = r.as_quat()
+                brick_T[:3, :3] = r.as_matrix()
+                brick_T[:3, 3] = np.array([x, y, z])
+                while(not collision_free_env(k, brick_T, self.objects_T, self.lego_lib, P_len=self.P_len)):
+                    x, y = np.random.uniform(self.workspace_range["x_min"], self.workspace_range["x_max"]), np.random.uniform(self.workspace_range["y_min"], self.workspace_range["y_max"])
+                    brick_T[:3, 3] = np.array([x, y, z])
+                pose = [x, y, z, quat[0], quat[1], quat[2], quat[3]]
+                self.set_state(k, pose)
+            self.objects_T[k] = brick_T
 
     def set_state(self, name, pose):
         state_msg = ModelState()
--- src/py_scripts/utils.py
+++ src/py_scripts/utils.py
@@ -9,3 +9,48 @@ def load_json(fname):
     with open(fname, 'r') as file:
         data = json.load(file)
     return data
+
+def calc_brick_T(brick_name, x_on_plate, y_on_plate, z_on_plate, ori, plate_T, plate_dim, lego_lib, brick_tall=0.0096, P_len=0.008):
+    brick_id = brick_name[1:].split('_')[0]
+    brick_h, brick_width = lego_lib[brick_id]["height"], lego_lib[brick_id]["width"]
+    ref_pose = plate_T
+    topleft_offset = np.array([[1, 0, 0, -(plate_dim[0] * P_len) / 2.0],
+                                  [0, 1, 0, -(plate_dim[1] * P_len) / 2.0],
+                                  [0, 0, 1, 0],
+                                  [0, 0, 0, 1]])
+    brick_offset = np.array([[1, 0, 0, x_on_plate * P_len],
+                              [0, 1, 0, y_on_plate * P_len],
+                              [0, 0, 1, z_on_plate * brick_tall],
+                              [0, 0, 0, 1]])
+    brick_center_offset = np.array([[1, 0, 0, (brick_h * P_len) / 2.0],
+                                     [0, 1, 0, (brick_width * P_len) / 2.0],
+                                     [0, 0, 1, 0],
+                                     [0, 0, 0, 1]])
+    z_90 = np.array([[0, -1, 0, 0],
+                     [1, 0, 0, 0],
+                     [0, 0, 1, 0],
+                     [0, 0, 0, 1]])
+    
+    brick_pose = ref_pose @ topleft_offset @ brick_offset @ brick_center_offset
+    if(ori == 1):
+        brick_center_offset[1, ] = -brick_center_offset[1, 3]
+        brick_pose = ref_pose @ topleft_offset @ brick_offset @ z_90 @ brick_center_offset
+    return brick_pose
+
+
+def collision_free_env(brick_name, brick_T, objects_T, lego_lib, eps=0.02, P_len=0.008):
+    brick_id = brick_name[1:].split('_')[0]
+    brick_h, brick_width = lego_lib[brick_id]["height"], lego_lib[brick_id]["width"]
+    brick_radius = 0.5 * np.sqrt((brick_h * P_len)**2 + (brick_width * P_len)**2)
+    for k in objects_T.keys():
+        other_T = objects_T[k]
+        if("plate" in k):
+            other_radius = 0.5 * np.sqrt((48 * P_len)**2 + (48 * P_len)**2)
+        else:
+            other_id = k[1:].split('_')[0]
+            other_h, other_width = lego_lib[other_id]["height"], lego_lib[other_id]["width"]
+            other_radius = 0.5 * np.sqrt((other_h * P_len)**2 + (other_width * P_len)**2)
+        dist = np.linalg.norm(brick_T[:3, 3] - other_T[:3, 3])
+        if(dist < (brick_radius + other_radius + eps)):
+            return False
+    return True
